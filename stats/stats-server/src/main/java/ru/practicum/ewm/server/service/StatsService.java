9e217c731f6c4906a5ed847b61a5902534e0e9d3
tubashov <tubashov@yandex.ru>
2025-12-29 17:29:02 +0300

    Update StatsService

--- stats/stats-server/src/main/java/ru/practicum/ewm/server/service/StatsService.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/server/service/StatsService.java
@@ -2,7 +2,6 @@ package ru.practicum.ewm.server.service;
 
 import ru.practicum.ewm.stats.dto.EndpointHitDto;
 import ru.practicum.ewm.stats.dto.ViewStatsDto;
-import ru.practicum.ewm.server.model.EndpointHit;
 
 import java.time.LocalDateTime;
 import java.util.List;
