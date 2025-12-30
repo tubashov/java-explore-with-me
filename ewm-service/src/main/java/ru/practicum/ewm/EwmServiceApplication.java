42a43d7f5574e898bbd2105515fb5850de849414
tubashov <tubashov@yandex.ru>
2025-12-30 14:17:12 +0300

    Changing the structure of the main service

--- ewm-service/src/main/java/ru/practicum/ewm/EwmServiceApplication.java
+++ ewm-service/src/main/java/ru/practicum/ewm/EwmServiceApplication.java
@@ -1,11 +1,11 @@
-package ru.practicum.ewm.service.publicservice;
+package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PublicServiceApplication {
+public class EwmServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PublicServiceApplication.class, args);
+        SpringApplication.run(EwmServiceApplication.class, args);
     }
 }
