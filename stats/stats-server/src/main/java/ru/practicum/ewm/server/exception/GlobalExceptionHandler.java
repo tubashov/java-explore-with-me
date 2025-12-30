66718ff7d1b46e781980c5fc78e4d45522545d33
tubashov <tubashov@yandex.ru>
2025-12-30 20:51:25 +0300

    Error correction

--- stats/stats-server/src/main/java/ru/practicum/ewm/server/exception/GlobalExceptionHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/server/exception/GlobalExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
