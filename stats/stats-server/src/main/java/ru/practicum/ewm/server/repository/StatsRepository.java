70390cc7a5dcf7182ca2ad8940b81dc9c0e09b85
tubashov <tubashov@yandex.ru>
2026-01-03 13:38:28 +0300

    Update StatsRepository

--- stats/stats-server/src/main/java/ru/practicum/ewm/server/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/server/repository/StatsRepository.java
@@ -10,30 +10,35 @@ import java.util.List;
 
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
-    @Query("SELECT new ru.practicum.ewm.stats.dto.ViewStatsDto(" +
-            "MIN(h.app), " +
-            "h.uri, " +
-            "COUNT(h.id)) " +
-            "FROM EndpointHit h " +
-            "WHERE h.timestamp BETWEEN :start AND :end " +
-            "AND (:uris IS NULL OR h.uri IN :uris) " +
-            "GROUP BY h.uri " +
-            "ORDER BY COUNT(h.id) DESC")
+    @Query("""
+            SELECT new ru.practicum.ewm.stats.dto.ViewStatsDto(
+                MIN(h.app),
+                h.uri,
+                COUNT(h.id)
+            )
+            FROM EndpointHit h
+            WHERE h.timestamp BETWEEN :start AND :end
+              AND (:uris IS NULL OR h.uri IN :uris)
+            GROUP BY h.uri
+            ORDER BY COUNT(h.id) DESC
+            """)
     List<ViewStatsDto> findStats(LocalDateTime start,
                                  LocalDateTime end,
                                  List<String> uris);
 
-    @Query("SELECT new ru.practicum.ewm.stats.dto.ViewStatsDto(" +
-            "MIN(h.app), " +
-            "h.uri, " +
-            "COUNT(DISTINCT h.ip)) " +
-            "FROM EndpointHit h " +
-            "WHERE h.timestamp BETWEEN :start AND :end " +
-            "AND (:uris IS NULL OR h.uri IN :uris) " +
-            "GROUP BY h.app, h.uri " +
-            "ORDER BY COUNT(DISTINCT h.ip) DESC")
+    @Query("""
+            SELECT new ru.practicum.ewm.stats.dto.ViewStatsDto(
+                MIN(h.app),
+                h.uri,
+                COUNT(DISTINCT h.ip)
+            )
+            FROM EndpointHit h
+            WHERE h.timestamp BETWEEN :start AND :end
+              AND (:uris IS NULL OR h.uri IN :uris)
+            GROUP BY h.app, h.uri
+            ORDER BY COUNT(DISTINCT h.ip) DESC
+            """)
     List<ViewStatsDto> findStatsUnique(LocalDateTime start,
                                        LocalDateTime end,
                                        List<String> uris);
-
 }
